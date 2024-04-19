6f6f2d18de88b8f272e8ea8ec982c220693a8915
Felix Luciano Salomon <felixluciano.a@gmail.com>
2024-04-19 00:39:12 -0700

    Added chain.save chain.custom to save your current chain configuration and set later

---
+++ functions/chain.custom.fish
@@ -0,0 +1,3 @@
+function chain.custom -d 'Sets chain to saved custom chain'
+    set -U chain_links $custom_chain
+end
---
+++ functions/chain.save.fish
@@ -0,0 +1,3 @@
+function chain.save -d 'Saves current custom chain'
+    set -U custom_chain $chain_links
+end
